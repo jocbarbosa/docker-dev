980fa3459b1f50477e52ef32904c108b75ac1475
Joao C Barbosa <joaao@outlook.com>
2020-05-29 23:59:49 -0300

    logging info

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import getpass
 
 class MyHttpHandler(http.server.SimpleHTTPRequestHandler):
     def log_message(self, format, *args):
-        logging.info("%s - - [%s] %s\n"%(
+        logging.info("%s - - [%s] %s\n"% (
             self.client_address[0],
             self.log_date_time_string(),
             format%args
@@ -14,4 +14,15 @@ class MyHttpHandler(http.server.SimpleHTTPRequestHandler):
 logging.basicConfig(
     filename='/log/http-server.log',
     format='%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.INFO
 )
+
+logging.getLogger().addHandler(logging.StreamHandler())
+logging.info('Initializing...')
+PORT = 8000
+
+httpd = socketserver.TCPServer(("", PORT), MyHttpHandler)
+logging.info('listening port : %s', PORT)
+logging.info('user: %s', getpass.getuser())
+
+httpd.serve_forever()
